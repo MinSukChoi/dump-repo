31193264d748271ee5faf82a711702a0a5ce8b87
최민석 <min@choemineoguiMBP.lan>
2019-04-16 16:01:23 +0900

    * make send button works

--- app/src/main/java/com/clab/neo/neoclient/MainActivity.java
+++ app/src/main/java/com/clab/neo/neoclient/MainActivity.java
@@ -2,6 +2,7 @@ package com.clab.neo.neoclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_send).setOnClickListener(v -> {
+            v.setVisibility(View.GONE);
+            findViewById(R.id.text_view_plain1).setVisibility(View.GONE);
+            findViewById(R.id.group_result_text).setVisibility(View.VISIBLE);
+        });
     }
 }
