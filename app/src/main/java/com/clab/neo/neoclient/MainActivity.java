029f161d636b33b109142230b6327e6914967ecb
최민석 <min@choemineoguiMBP.lan>
2019-04-16 16:27:34 +0900

    * add basic interaction

--- app/src/main/java/com/clab/neo/neoclient/MainActivity.java
+++ app/src/main/java/com/clab/neo/neoclient/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,9 +15,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.button_send).setOnClickListener(v -> {
+            findViewById(R.id.progress_main).setVisibility(View.VISIBLE);
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    MainActivity.this.runOnUiThread(() -> {
+                        findViewById(R.id.progress_main).setVisibility(View.GONE);
+                        v.setVisibility(View.GONE);
+                        findViewById(R.id.group_initial_text).setVisibility(View.GONE);
+                        findViewById(R.id.group_result_text).setVisibility(View.VISIBLE);
+                    });
+                }
+            }, 2000);
+        });
+
+        findViewById(R.id.button_retry).setOnClickListener(v -> {
             v.setVisibility(View.GONE);
-            findViewById(R.id.text_view_plain1).setVisibility(View.GONE);
-            findViewById(R.id.group_result_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.group_initial_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.group_result_text).setVisibility(View.GONE);
         });
     }
 }
